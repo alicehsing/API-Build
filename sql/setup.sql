06e24bf506e4faddeb539cc738b94aeb90d8e9c3
Alice Hsing <alicehsing@gmail.com>
2022-03-15 15:46:12 -0700

    added SQL table

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,12 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+DROP TABLE IF EXISTS cars;
+
+CREATE TABLE cars (
+    id BIGINT GENERATED ALWAYS AS IDENTITY,
+    make TEXT NOT NULL,
+    type TEXT NOT NULL,
+    model TEXT NOT NULL,
+    year INT NOT NULL
+);
