ea59b40af335f9571aab44c3aa609c605a6a300a
Alice Hsing <alicehsing@gmail.com>
2022-03-15 18:20:28 -0700

    added GET ALL entries endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Car = require('../lib/models/Car');
 
 describe('API-Build routes', () => {
   beforeEach(() => {
@@ -15,7 +16,7 @@ describe('API-Build routes', () => {
 it('should create a car', async () => {
   const res = await request(app)
   .post('/api/v1/cars')
-  .send({ make: 'Audi', type: 'convertible', model: 'Cabriolet', yearReleased: 2015});
+  .send({ make: 'Audi', type: 'convertible', model: 'Cabriolet', yearReleased: 2015 });
 
   expect(res.body).toEqual({
     id: expect.any(String),
@@ -24,6 +25,36 @@ it('should create a car', async () => {
     model: 'Cabriolet', 
     yearReleased: 2015
   });
+})
+
+it('should be able to list an array of cars', async() => {
+  await Car.insert({ make: 'Audi', type: 'convertible', model: 'Cabriolet', yearReleased: 2015 });
+  const res = await request(app)
+  .get('/api/v1/cars');
+
+  expect(res.body).toEqual([
+    {
+      id: expect.any(String),
+      make: "Mercedes",
+      type: "SUV",
+      model: "G-Class",
+      yearReleased: 2021,
+    },
+    {
+      id: expect.any(String),
+      make: "Ford",
+      type: "coupe",
+      model: "Mustang GT",
+      yearReleased: 2017,
+    },
+    {
+      id: expect.any(String),
+      make: 'Audi', 
+      type: 'convertible', 
+      model: 'Cabriolet', 
+      yearReleased: 2015
+    }, 
+  ])
 })
 
 });
